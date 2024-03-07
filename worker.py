2e2aa8c88cf5050455527d0f26c00bfdb8daeafd
Arifian Saputra - AAI @ IL <161233629+arifian-il-aai@users.noreply.github.com>
2024-03-07 11:40:56 +0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -23,7 +23,7 @@ llm_embeddings = None
 def init_llm():
     global llm, llm_embeddings
     # Initialize the language model with the OpenAI API key
-    api_key="YOUR API KEY"
+    api_key="YOUR_API_KEY"
     # ---> TODO: write your code here <----
     llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key)
     # Initialize the embeddings for the language model
